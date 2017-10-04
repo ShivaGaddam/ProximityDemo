c6eef506f50356a06e37de855a391525ec24c648
Madhukar <Madhukar@Madhukars-MacBook-Pro.local>
2017-10-04 02:30:58 -0600

    Initial deployment

---
+++ ProximityDemo/ProximityDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProximityDemo
+//
+//  Created by Madhukar Bommala on 10/4/17.
+//  Copyright © 2017 Madhukar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("made some change")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
